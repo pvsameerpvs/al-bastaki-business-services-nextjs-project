568e04045ebe35433aecc89455f35b2fb43cf6c1
pvsameerpvs <pvsameerpvs@gmail.com>
2026-03-28 16:10:45 +0400

    fix: ui currct

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="relative">
       <body className={rethinkSans.className}>
         <CustomCursor />
         <SmoothScroller>
